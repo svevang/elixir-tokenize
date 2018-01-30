b92f2c0a3be8e1f8b7587ca376ffe11c9a3361de
Sam Vevang <sam.vevang@gmail.com>
2018-01-30 15:45:51 -0600

    initial ci

---
+++ lib/tokenize.ex
@@ -0,0 +1,18 @@
+defmodule Tokenize do
+  @moduledoc """
+  Documentation for Tokenize.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tokenize.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tokenize_test.exs
@@ -0,0 +1,8 @@
+defmodule TokenizeTest do
+  use ExUnit.Case
+  doctest Tokenize
+
+  test "greets the world" do
+    assert Tokenize.hello() == :world
+  end
+end
